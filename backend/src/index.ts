3fa197de1c9a3650da0af43dee1ffb26a3e3314a
Valentin Svirskiy <vsvirskiy98@gmail.com>
2025-07-07 16:28:37 +0300

    Add router '/api/product' to application.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+import productRouter from './routers/product-router';
+import ApiError from './errors/error-api';
 
 dotenv.config();
 
@@ -8,6 +11,15 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+
+// routers
+app.use('/api/product', productRouter);
+
+// error handling
+app.use((err: ApiError, req: Request, res: Response, next: Function) => {
+    res.status(err.status).json({message: err.message});
+    next();
+});
 
 try {
     app.listen(PORT, () => console.log(`Server starts on ${PORT}`));
