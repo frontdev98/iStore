192a6a629b02924b9f9ffb6ce41587885716ca9f
Valentin Svirskiy <vsvirskiy98@gmail.com>
2025-07-07 15:12:53 +0300

    Create product service & dto.

---
+++ backend/src/dtos/product-dto.ts
@@ -0,0 +1,5 @@
+export default interface productDTO {
+    name: string,
+    price: number,
+    year: Date
+}
---
+++ backend/src/services/product-service.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from "../../prisma/src/generated/prisma";
+import ApiError from "../errors/error-api";
+import productDTO from "../dtos/product-dto";
+
+const db = new PrismaClient();
+
+class ProductService {
+    async getAll(): Promise<productDTO[] | []> {
+        const products = await db.product.findMany({
+            select: {
+                name: true,
+                price: true,
+                year: true
+            }
+        });
+        return products;
+    }
+
+    async getOne(id: number): Promise<productDTO> {
+        const product_ = await db.product.findUnique({
+            where: {id},
+            select: {
+                name: true,
+                price: true,
+                year: true
+            }
+        });
+
+        if (product_ === null) {
+            throw ApiError.NotFound();
+        }
+
+        return product_;
+    }
+
+    async create(data: productDTO): Promise<productDTO> {
+        const product_ = await db.product.create({data});
+        const dto = {...product_};
+        return dto;
+    }
+
+    async update(data: productDTO): Promise<productDTO> {
+        const product_ = await db.product.update({
+            where: {
+                name: data.name
+            },
+            data
+        });
+        const dto = {...product_};
+        return dto;
+    }
+
+    async delete(name: string): Promise<productDTO> {
+        const product_ = await db.product.delete({
+            where: {name}
+        });
+        const dto = {...product_};
+        return dto;
+    }
+}
+
+const productService = new ProductService();
+
+export default productService;
