a7d0b022e204be62468a336b84c7cb5e1dbc0070
Valentin Svirskiy <vsvirskiy98@gmail.com>
2025-07-07 09:38:16 +0300

    Initialize dotenv.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -6,4 +9,9 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server starts on ${PORT}`));
+try {
+    app.listen(PORT, () => console.log(`Server starts on ${PORT}`));
+    
+} catch (e) {
+    console.log(e)
+}
